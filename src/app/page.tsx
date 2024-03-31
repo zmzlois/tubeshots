4fa94a3fa070a3a36497fb9f3342c2f70190ed7d
zmzlois <lois.z@comcord.vision>
2024-03-31 20:23:29 +0100

    remove cloudflare in tsconfig

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Form from "@/components/form";
 export default function Home() {
   return (
-    <main className="flex h-screen border-2 border-red-400 content-center flex-col justify-center items-center p-24">
+    <main className="flex h-screen content-center flex-col justify-center items-center p-24">
       <div className="p-10 container mx-auto flex flex-col border-2 gap-10">
         <h1 className="text-3xl font-bold">
           Generate consecutive screenshots from Vimeo
--- src/components/form.tsx
+++ src/components/form.tsx
@@ -4,7 +4,10 @@ import { Input } from "./ui/input";
 export default function Form() {
   return (
     <form className="flex flex-col gap-10">
-      <Input placeholder="Paste vimeo URL here" />
+      <Input
+        placeholder="Paste vimeo URL here"
+        pattern="(https?|ftp):\/\/[^\s/$.?#].[^\s]*"
+      />
       <Button>
         <p className="text-zinc-100"> Create screen shots</p>
       </Button>
