c489b7d9c0faadae9e8f40bb693f3d11ca9b44c9
zmzlois <lois.z@comcord.vision>
2024-03-31 17:44:32 +0100

    update package json

--- src/app/api/image/route.ts
+++ src/app/api/image/route.ts
@@ -18,14 +18,19 @@ export async function GET(req: Request, res: Response) {
   let browser;
 
   try {
-    browser = await puppeteer.launch({ headless: false });
+    browser = await puppeteer.launch({
+      headless: false, args: [
+        "--start-fullscreen",
+      ]
+    });
     const page = await browser.newPage();
-    await page.setViewport({ width: 1366, height: 768 })
+
+    await page.setViewport({ width: 3024, height: 1964 })
     await page.goto(url);
 
-    const closeButton = await page.waitForSelector(".sc-iipuKH > button")
+    // const closeButton = await page.waitForSelector(".sc-iipuKH > button")
 
-    if (closeButton) await closeButton.click()
+    //if (closeButton) await closeButton.click()
 
     const fullScreen = await page.$("button[data-enter-fullscreen='true']")
 
@@ -35,7 +40,6 @@ export async function GET(req: Request, res: Response) {
     // click on the play button
     if (playButton) playButton.click()
 
-    const removeFocus = await page.$(".video_attributes > span")
     const video = await page.waitForSelector("video")
 
     if (!video) {
@@ -47,11 +51,6 @@ export async function GET(req: Request, res: Response) {
       )
 
     }
-
-    //move the focus to somewhere other than the video to remove the play bar 
-
-    if (removeFocus) await removeFocus.focus()
-
 
     let screenshot
 
@@ -85,7 +84,7 @@ export async function GET(req: Request, res: Response) {
     );
   } finally {
     if (browser) {
-      // await browser.close();
+      await browser.close();
     }
   }
 
